4e64a7b4b4917b934d7da3573378330685a8a6bd
emmadino221 <augustineemmanuel221@gmail.com>
2025-08-19 10:49:22 +0200

    javascript code refactorize

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ const resultCalculation = document.querySelector(".result-calculation");
 const btn = document.querySelector("button");
 const clear = document.querySelector(".clear");
 
-btn.addEventListener("click", function () {
+// function for the monthly payment
+
+function monthlyPayment() {
   const loan = Number(loanAmount.value);
   const loanYears = Number(loanTermyears.value);
   const interestRate = Number(annualInterestRate.value);
@@ -15,35 +17,47 @@ btn.addEventListener("click", function () {
   const monthlyInterestRate = interestRate / 100 / 12;
   const numberOfPayment = loanYears * 12;
 
-  const totalMonthlypayment = monthlyPayment(
-    loan,
-    monthlyInterestRate,
-    numberOfPayment
-  );
-
-  document.querySelector(".result-cal").textContent = "$" + totalMonthlypayment;
-  const totalAmount = numberOfPayment * totalMonthlypayment;
-  document.querySelector("h6").textContent = "$" + totalAmount.toFixed(2);
-
-  emptyResult.classList.toggle("hidden");
-  resultCalculation.classList.toggle("hidden");
-});
-
-clear.addEventListener("click", function () {
-  document.getElementById("loan").value = "";
-  document.getElementById("mortage").value = "";
-  document.getElementById("interest").value = "";
-  resultCalculation.classList.add("hidden");
-  emptyResult.classList.remove("hidden");
-});
-
-// function for the monthly payment
-
-function monthlyPayment(loanAmount, monthlyInterestRate, numberOfPayment) {
   const calculation =
-    (loanAmount *
+    (loan *
       monthlyInterestRate *
       Math.pow(1 + monthlyInterestRate, numberOfPayment)) /
     (Math.pow(1 + monthlyInterestRate, numberOfPayment) - 1);
-  return calculation.toFixed(2);
+
+  const totalMonthlypayment = calculation;
+  return totalMonthlypayment.toFixed(2);
 }
+
+function numPayment() {
+  const numberOfPayment = Number(loanTermyears.value) * 12;
+  return numberOfPayment.toFixed(2);
+}
+
+// function to toggle empty resuly and result calculation
+
+function result() {
+  emptyResult.classList.toggle("hidden");
+  resultCalculation.classList.toggle("hidden");
+}
+
+// function to clear all data
+
+function clearData() {
+  document.getElementById("loan").value = "";
+  document.getElementById("mortage").value = "";
+  document.getElementById("interest").value = "";
+  resultCalculation.classList.toggle("hidden");
+  emptyResult.classList.toggle("hidden");
+}
+
+// button clicked functionality
+
+btn.addEventListener("click", function () {
+  document.querySelector(".result-cal").textContent = "$" + monthlyPayment();
+  const totalAmount = numPayment() * monthlyPayment();
+  document.querySelector("h6").textContent = "$" + totalAmount.toFixed(2);
+  result();
+});
+
+clear.addEventListener("click", function () {
+  clearData();
+});
